e4ab5f1095ed22fea31b29224d53a1ad34f4a44f
mokshatrey <atreymoksh1999@gmail.com>
2023-11-15 08:53:06 +0530

    Initial commit

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,60 @@
+import 'dart:io'; // Add this import for File class
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key,
+  required this.onPickImage,
+  }) ;// Fixed the constructor
+
+  final void Function(File pickedImage) onPickImage;
+
+  @override
+  State<UserImagePicker> createState() {
+    return _UserImagePickerState();
+  }
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImageFile; // Fixed the data type
+
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _pickedImageFile = File(pickedImage.path); // Create a File object
+    });
+
+    widget.onPickImage(_pickedImageFile!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage, // Call the _pickImage function
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
